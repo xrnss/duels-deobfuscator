a11f35d2a1796a0e80cfa6facdc6466d59c046c6
ross <ross.hamilton7@icloud.com>
2021-08-16 14:42:55 +0100

    Change version
    forgot to change it in main file oopsie

--- src/main/java/com/rnss/duelsdeobf/Deobfuscator.java
+++ src/main/java/com/rnss/duelsdeobf/Deobfuscator.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 @Mod(modid = Deobfuscator.MODID, version = Deobfuscator.VERSION, acceptedMinecraftVersions="[1.8.9]")
 public class Deobfuscator {
     public static final String MODID = "duelsdeobf";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.2";
 
     private boolean hypixel = false;
     private boolean isToggled = true; // Default to on
